f33d97b1f5de363415aa52a7bc32e4d5283bff32
Alexxq64 <alexxq@list.ru>
2025-05-25 08:54:25 +0300

    language changed from en to ru

--- fuel_manager/fuel/admin.py
+++ fuel_manager/fuel/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.apps import apps
 from django.contrib.admin.sites import AlreadyRegistered
 
+# Настройка заголовков админки
+admin.site.site_header = "АЗС Администратор"
+admin.site.site_title = "АЗС Администрирование"
+admin.site.index_title = "Панель управления АЗС"
+
+# Автоматическая регистрация всех моделей из приложения 'fuel'
 app_models = apps.get_app_config('fuel').get_models()  
 
 for model in app_models:
